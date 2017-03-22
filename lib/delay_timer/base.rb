3f1d2368f06843fe91800ceda19ff0e3dcc80143
Kenneth Leung <kenneth@leungs.us>
2017-03-22 09:10:56 -0700

    using DateTime instead of TIme to allow delay more than 24 hours.

--- lib/delay_timer/base.rb
+++ lib/delay_timer/base.rb
@@ -5,7 +5,7 @@ class DelayTimer
   class Base
     attr_reader :now, :delay_until, :logger, :debug
 
-    def initialize(delay_until:, now: Time.now, logger: Logger.new($stderr), debug: false)
+    def initialize(delay_until:, now: DateTime.now, logger: Logger.new($stderr), debug: false)
       @now         = now
       @delay_until = delay_until
       @logger      = logger
@@ -13,11 +13,17 @@ class DelayTimer
     end
 
     def start
-      wait        = Time.at(lapse_seconds).utc.strftime('%H:%M:%S')
-      target_time = specified_time.strftime('%H:%M:%S')
-
-      msg = ["Time now: #{now.strftime('%H:%M:%S')}.", "Target: #{target_time}.", "Wait: #{wait}."].join(' ')
-      logger.info msg
+      if lapse_seconds > (24 * 60 * 60)
+        now_time    = now.strftime('%c')
+        target_time = specified_time.strftime('%c')
+        wait = Time.at(lapse_seconds).utc.to_datetime.prev_day.strftime('%d:%H:%M:%S')
+      else
+        now_time    = now.strftime('%H:%M:%S')
+        target_time = specified_time.strftime('%H:%M:%S')
+        wait = Time.at(lapse_seconds).utc.to_datetime.strftime('%H:%M:%S')
+      end
+      msg = ["Time now: #{now_time}.", "Target: #{target_time}.", "Wait: #{wait}."].join(' ')
+      logger.info msg unless debug
 
       pause(lapse_seconds) unless debug
     end
@@ -27,10 +33,10 @@ class DelayTimer
         case delay_until
         when String
           Time.parse(delay_until)
-        when Time
+        when Time, DateTime
           delay_until
         when Date
-          delay_until.to_time
+          delay_until.to_datetime
         else
           raise "Don't know how to handle #{delay_until.class}!"
         end
@@ -41,7 +47,7 @@ class DelayTimer
       @lapse_seconds ||= begin
         time = specified_time
         time = time.to_datetime.next_day.to_time if specified_time_has_passed?
-        (time - now).ceil.to_i
+        ((time - now)*24*60*60).ceil.to_i
       end
     end
 
--- spec/delay_timer_spec.rb
+++ spec/delay_timer_spec.rb
@@ -3,16 +3,35 @@ require_relative '../lib/delay_timer'
 require 'pp'
 
 describe DelayTimer do
-  let(:adj_minutes) { 3 * 60 }
-  let(:now) { Time.now }
-  let(:timer) { DelayTimer.create(:delay_until => (now + adj_minutes), :debug => true) }
-  it '#specified_time' do
-    expect(timer.specified_time).to eq(now + adj_minutes)
+  context 'regular usage (within 24 hours)' do
+    let(:minutes_in_day) { 1 * 24 * 60 }
+    let(:adj_minutes) { Rational(3, minutes_in_day) }
+    let(:now) { DateTime.now }
+    let(:timer) { DelayTimer.create(:delay_until => (now + adj_minutes), :debug => true) }
+    it '#specified_time' do
+      expect(timer.specified_time).to eq(now + adj_minutes)
+    end
+    it '#total_seconds' do
+      expect(timer.total_seconds).to eq(180)
+    end
+    it '#start' do
+      timer.start
+    end
   end
-  it '#total_seconds' do
-    expect(timer.total_seconds).to eq(180)
-  end
-  it '#start' do
-    timer.start
+  context 'wait more than 24 hours' do
+    let(:time) { DateTime.now.next_day(7) }
+    let(:timer) { DelayTimer.create(:delay_until => time, :debug => true) }
+    it '#specified_time' do
+      expect(timer.specified_time).to eq(time)
+    end
+    it '#specified_time_has_passed?' do
+      expect(timer.send(:specified_time_has_passed?)).to be_falsey
+    end
+    it '#total_seconds' do
+      expect(timer.total_seconds).to eq(604800)
+    end
+    it '#start' do
+      timer.start
+    end
   end
 end
